312331d101d8da8652a2609690dba69f7a153610
Harsh Vyapari <51878195+harshv5094@users.noreply.github.com>
2023-09-16 21:39:04 +0530

    feat: Designing addToFavorite function and styling html

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,24 @@
 import { useContext } from "react"
 import { BookContext } from "../components/BooksProvider"
+// import { NavLink } from "react-router-dom"
 
 function Home() {
-  const { book } = useContext(BookContext)
+  const { book, favoriteBook } = useContext(BookContext)
   const { books, getBooks } = book
+  const { setFavoriteBooks } = favoriteBook
 
-  console.log(books)
+  const addToFavorite = (itemID) => {
+    const bookToAdd = books.find(item => item.id === itemID)
+
+    setFavoriteBooks((previousBooks) => {
+      if (previousBooks.length !== 0) {
+        return [...previousBooks, bookToAdd]
+      }
+      else {
+        return [bookToAdd]
+      }
+    })
+  }
 
   const markAsRead = (itemID) => {
     getBooks(books.map(item => {
@@ -16,13 +29,14 @@ function Home() {
     }))
   }
 
+
   return (
     <div className="home">
       <div className="title">
         All Books
       </div>
       <div className="content">
-        <div className="book-list">
+        <section className="book-list">
           {
             books.map((item, index) => {
               return (
@@ -30,7 +44,7 @@ function Home() {
                   <section className="book-image">
                     <img width={"200px"} height={"300px"} src={`${item.image}`} alt={`${item.title}`} />
                   </section>
-                  <div className="book-content">
+                  <section className="book-content">
                     <strong>Title:</strong> {item.title}
                     <br />
                     <br />
@@ -38,12 +52,14 @@ function Home() {
                     <br />
                     <br />
                     {item.read === false ? (<button onClick={() => markAsRead(item.id)}>Mark as read</button>) : (<button disabled>Already Read</button>)}
-                  </div>
+                    <br />
+                    <button onClick={() => addToFavorite(item.id)}>Add To Favorite</button>
+                  </section>
                 </div>
               )
             })
           }
-        </div>
+        </section>
       </div>
     </div>
   )
